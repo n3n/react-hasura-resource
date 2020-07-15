5c19e072e3cb8eae657dc2bd41160173ca7c8a0d
Nonpawit Teerachetmongkol <nonpawit.tee@gmail.com>
2020-07-15 10:27:25 +0700

    Add to support nested where

--- src/props.ts
+++ src/props.ts
@@ -2,33 +2,51 @@ import { QueryOptions } from 'react-query';
 
 export type FetchStatus = 'loading' | 'success' | 'error' | 'idle';
 
-export type HasuraResourceRenderProps<T = object> = {
+export type Operator = '_eq' |  '_neq' |  '_gt' |  '_lt' |  '_gte' |  '_lte' |  '_in' |  '_nin' | '_like' | '_nlike' | '_ilike' | '_nilike' | '_similar' | '_nsimilar' | '_is_null' | '_cast' | '_contains' | '_contained_in' | '_has_key' | '_has_keys_any' | '_has_keys_all' | '_st_contains' | '_st_crosses' | '_st_equals' | '_st_intersects' | '_st_overlaps' | '_st_touches' | '_st_within' | '_st_d_within' | '_st_intersects_rast' | '_st_intersects_nband_geom' | '_st_intersects_geom_nband'
+
+export interface BaseHasuraRenderProps<T> {
   data: T | undefined;
   status: FetchStatus;
   error?: Error | null;
 };
 
-export interface HasuraResourceListRenderProps<T = object[]> extends HasuraResourceRenderProps<T> {
-  totalCount: number;
+export type HasuraQueryWhereOperator = {
+  [key in Operator]: Partial<HasuraQueryWherePartial>;
 }
 
-export type HasuraQueryColumnRelationship = {
+export type HasuraQueryWhereString = {
+  [key: string]: Partial<HasuraQueryWherePartial>;
+}
+
+export type HasuraQueryWherePartial = string | HasuraQueryWhereOperator | HasuraQueryWhereString
+
+export type HasuraQueryWhere = Partial<{
+  [key: string]: Partial<HasuraQueryWherePartial>;
+}>;
+
+export interface HasuraQueryArgs {
+  where?: HasuraQueryWhere;
+  columns?: HasuraQueryColumn[];
+};
+
+export interface HasuraQueryColumnRelationship {
   name: string;
   columns: HasuraQueryColumn[];
+  alias?: string;
+  where?: HasuraQueryWhere;
 };
 
 export type HasuraQueryColumn = string | HasuraQueryColumnRelationship;
 
-export type HasuraQueryArgs = {
-  where?: {
-    [key: string]: string;
-  };
-  columns?: HasuraQueryColumn[];
-};
-
 // Resource
 
-export type HasuraResourceProps = {
+export interface HasuraResourceRenderProps<T extends Object = object> extends BaseHasuraRenderProps<T> {
+  data: T | undefined;
+  status: FetchStatus;
+  error?: Error | null;
+};
+
+export interface HasuraResourceProps {
   id: string | number;
   tableName: string;
   args?: HasuraQueryArgs;
@@ -36,7 +54,7 @@ export type HasuraResourceProps = {
   queryOptions?: QueryOptions<object>;
 };
 
-export type BaseGetResourceHookProps = {
+export interface BaseGetResourceHookProps {
   id: string | number;
   tableName: string;
   args?: HasuraQueryArgs;
@@ -50,6 +68,10 @@ export interface GetResourceFnProps extends BaseGetResourceHookProps {
 
 // Resource List
 
+export interface HasuraResourceListRenderProps<T extends any[] | [] = object[]> extends BaseHasuraRenderProps<T> {
+  totalCount: number;
+}
+
 export interface HasuraQueryListArgs extends HasuraQueryArgs {
   pagination?: {
     perPage: number;
@@ -61,14 +83,14 @@ export interface HasuraQueryListArgs extends HasuraQueryArgs {
   };
 }
 
-export type HasuraResourceListProps = {
+export interface HasuraResourceListProps {
   tableName: string;
   render: (props: HasuraResourceListRenderProps) => JSX.Element;
   queryOptions?: QueryOptions<[object[], { count: number }]>;
   args?: HasuraQueryListArgs;
 };
 
-export type BaseGetResourceListHookProps = {
+export interface BaseGetResourceListHookProps {
   tableName: string;
   args?: HasuraQueryListArgs;
 };

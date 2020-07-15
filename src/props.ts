cd51f40d4253bc5ee9db1ed4e70c3ea9da6c0a14
Nonpawit Teerachetmongkol <nonpawit.tee@gmail.com>
2020-07-15 10:30:10 +0700

    Fix lint

--- src/props.ts
+++ src/props.ts
@@ -2,23 +2,55 @@ import { QueryOptions } from 'react-query';
 
 export type FetchStatus = 'loading' | 'success' | 'error' | 'idle';
 
-export type Operator = '_eq' |  '_neq' |  '_gt' |  '_lt' |  '_gte' |  '_lte' |  '_in' |  '_nin' | '_like' | '_nlike' | '_ilike' | '_nilike' | '_similar' | '_nsimilar' | '_is_null' | '_cast' | '_contains' | '_contained_in' | '_has_key' | '_has_keys_any' | '_has_keys_all' | '_st_contains' | '_st_crosses' | '_st_equals' | '_st_intersects' | '_st_overlaps' | '_st_touches' | '_st_within' | '_st_d_within' | '_st_intersects_rast' | '_st_intersects_nband_geom' | '_st_intersects_geom_nband'
+export type Operator =
+  | '_eq'
+  | '_neq'
+  | '_gt'
+  | '_lt'
+  | '_gte'
+  | '_lte'
+  | '_in'
+  | '_nin'
+  | '_like'
+  | '_nlike'
+  | '_ilike'
+  | '_nilike'
+  | '_similar'
+  | '_nsimilar'
+  | '_is_null'
+  | '_cast'
+  | '_contains'
+  | '_contained_in'
+  | '_has_key'
+  | '_has_keys_any'
+  | '_has_keys_all'
+  | '_st_contains'
+  | '_st_crosses'
+  | '_st_equals'
+  | '_st_intersects'
+  | '_st_overlaps'
+  | '_st_touches'
+  | '_st_within'
+  | '_st_d_within'
+  | '_st_intersects_rast'
+  | '_st_intersects_nband_geom'
+  | '_st_intersects_geom_nband';
 
 export interface BaseHasuraRenderProps<T> {
   data: T | undefined;
   status: FetchStatus;
   error?: Error | null;
-};
+}
 
 export type HasuraQueryWhereOperator = {
   [key in Operator]: Partial<HasuraQueryWherePartial>;
-}
+};
 
 export type HasuraQueryWhereString = {
   [key: string]: Partial<HasuraQueryWherePartial>;
-}
+};
 
-export type HasuraQueryWherePartial = string | HasuraQueryWhereOperator | HasuraQueryWhereString
+export type HasuraQueryWherePartial = string | HasuraQueryWhereOperator | HasuraQueryWhereString;
 
 export type HasuraQueryWhere = Partial<{
   [key: string]: Partial<HasuraQueryWherePartial>;
@@ -27,14 +59,14 @@ export type HasuraQueryWhere = Partial<{
 export interface HasuraQueryArgs {
   where?: HasuraQueryWhere;
   columns?: HasuraQueryColumn[];
-};
+}
 
 export interface HasuraQueryColumnRelationship {
   name: string;
   columns: HasuraQueryColumn[];
   alias?: string;
   where?: HasuraQueryWhere;
-};
+}
 
 export type HasuraQueryColumn = string | HasuraQueryColumnRelationship;
 
@@ -44,7 +76,7 @@ export interface HasuraResourceRenderProps<T extends Object = object> extends Ba
   data: T | undefined;
   status: FetchStatus;
   error?: Error | null;
-};
+}
 
 export interface HasuraResourceProps {
   id: string | number;
@@ -52,13 +84,13 @@ export interface HasuraResourceProps {
   args?: HasuraQueryArgs;
   render: (props: HasuraResourceRenderProps) => JSX.Element;
   queryOptions?: QueryOptions<object>;
-};
+}
 
 export interface BaseGetResourceHookProps {
   id: string | number;
   tableName: string;
   args?: HasuraQueryArgs;
-};
+}
 
 export interface GetResourceFnProps extends BaseGetResourceHookProps {
   endpoint: string;
@@ -88,12 +120,12 @@ export interface HasuraResourceListProps {
   render: (props: HasuraResourceListRenderProps) => JSX.Element;
   queryOptions?: QueryOptions<[object[], { count: number }]>;
   args?: HasuraQueryListArgs;
-};
+}
 
 export interface BaseGetResourceListHookProps {
   tableName: string;
   args?: HasuraQueryListArgs;
-};
+}
 
 export interface GetResourceListFnProps extends BaseGetResourceListHookProps {
   endpoint: string;
